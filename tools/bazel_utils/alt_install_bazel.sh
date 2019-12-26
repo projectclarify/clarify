8c8d09bf077e0ccfc911ea4c2ed8901ee44232a2
cwbeitel <me@cwbeitel-github.com>
2019-12-26 02:26:51 +0000

    circle dev

--- tools/bazel_utils/alt_install_bazel.sh
+++ tools/bazel_utils/alt_install_bazel.sh
@@ -5,8 +5,8 @@ apt install g++ unzip zip wget -y
 
 apt-get install openjdk-11-jdk -y
 
-BAZEL_VERSION=2.0.0
-SCRIPT='bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh'
+BAZEL_VERSION="2.0.0"
+SCRIPT="bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
 
 cd $HOME && wget https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/$SCRIPT
 
