6558a9e61529fd3ee0ce377eb505a4eddc85f830
cwbeitel <me@cwbeitel-github.com>
2020-01-08 02:00:56 +0000

    debug testing

--- tools/testing/test_circle.sh
+++ tools/testing/test_circle.sh
@@ -15,8 +15,9 @@
 
 # Usage: test_circle.sh ${CIRCLE_SHA1}
 
-export CMD="bash -c 'source ~/.bashrc; git checkout $1; pip install -r dev-requirements.txt; sh tools/testing/test_local.sh'"
+export CMD="docker run -it gcr.io/clarify/runtime-base:v0.1.0-799d \"bash -c 'source ~/.bashrc && git checkout $1 && pip install -r dev-requirements.txt --user && sh tools/testing/test_local.sh'\""
 
 echo $CMD
 
-docker run -it gcr.io/clarify/runtime-base:v0.1.0-799d $CMD
+exec $CMD
+
