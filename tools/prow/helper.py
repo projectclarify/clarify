c0dbf2d64bd5ca2e95fad09704c9490c8e0ec1bc
cwbeitel <me@cwbeitel-github.com>
2020-01-16 01:14:16 +0000

    prow dev

--- tools/prow/helper.py
+++ tools/prow/helper.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Prow deployment."""
+"""Prow deployment"""
 
 from __future__ import absolute_import
 from __future__ import division
