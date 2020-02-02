4a0768c16c534c7113248fa80afdad464a2deabc
Connor Kennedy <cpk2000x@gmail.com>
2020-02-01 18:53:43 -0800

    Style Issues 3

--- exp/golang-experiments/cpython-parallel/go-threading-test.py
+++ exp/golang-experiments/cpython-parallel/go-threading-test.py
@@ -2,12 +2,12 @@ from ctypes import c_long
 from ctypes import Structure
 from ctypes import cdll
 
-class TestClass(Structure):
+class test_class(Structure):
   _fields_ = [("t1", c_long), ("t2", c_long)]
 
 
 lib = cdll.LoadLibrary("./go-threading-test.so")
-obj = TestClass(100, 100)
+obj = test_class(100, 100)
 
-lib.Main.argtypes = [TestClass]
+lib.Main.argtypes = [test_class]
 lib.Main(obj)
